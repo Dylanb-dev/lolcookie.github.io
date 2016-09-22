e9c27816c1856b21c880bb9ac48457f303ca6ad2
Dylan <thatguy72@gmail.com>
2016-09-22 15:07:45 +0800

    bar chart achieved

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -1,86 +1,81 @@
 queue()
-    .defer(d3.json, "/api/data")
+    .defer(d3.json, "./data/data.json")
     .await(makeGraphs);
 
 function makeGraphs(error, apiData) {
-	
+  console.log(apiData[0]);
 //Start Transformations
 	var dataSet = apiData;
-	var dateFormat = d3.time.format("%m/%d/%Y");
 	dataSet.forEach(function(d) {
-		d.date_posted = dateFormat.parse(d.date_posted);
-				d.date_posted.setDate(1);
-		d.total_donations = +d.total_donations;
+		d.nightly_price = Number(d.nightly_price.replace(/[^0-9\.]+/g,""));
 	});
+  console.log(dataSet[0]);
 
 	//Create a Crossfilter instance
 	var ndx = crossfilter(dataSet);
 
 	//Define Dimensions
-	var datePosted = ndx.dimension(function(d) { return d.date_posted; });
-	var gradeLevel = ndx.dimension(function(d) { return d.grade_level; });
-	var resourceType = ndx.dimension(function(d) { return d.resource_type; });
-	var fundingStatus = ndx.dimension(function(d) { return d.funding_status; });
-	var povertyLevel = ndx.dimension(function(d) { return d.poverty_level; });
-	var state = ndx.dimension(function(d) { return d.school_state; });
-	var totalDonations  = ndx.dimension(function(d) { return d.total_donations; });
+	var nightlyPrice = ndx.dimension(function(d) { return d.nightly_price; });
+	var roomType = ndx.dimension(function(d) { return d.room_type; });
+	var ratingCheckin = ndx.dimension(function(d) { return d.rating_checkin; });
+	var satisfactionGuest = ndx.dimension(function(d) { return d.satisfaction_guest; });
+	var responseTime = ndx.dimension(function(d) { return d.responseTime; });
+	var personCapacity = ndx.dimension(function(d) { return d.person_capacity; });
+	var ratingCleanliness  = ndx.dimension(function(d) { return d.rating_cleanliness; });
 
 
 	//Calculate metrics
-	var projectsByDate = datePosted.group(); 
-	var projectsByGrade = gradeLevel.group(); 
-	var projectsByResourceType = resourceType.group();
-	var projectsByFundingStatus = fundingStatus.group();
-	var projectsByPovertyLevel = povertyLevel.group();
-	var stateGroup = state.group();
+	var propertiesByPrice = nightlyPrice.group();
+	var propertiesByType = roomType.group();
+	var propertiesByRatings = ratingCheckin.group();
+	var propertiesByGuests = satisfactionGuest.group();
+	var propertiesByCapacity = personCapacity.group();
 
 	var all = ndx.groupAll();
 
 	//Calculate Groups
-	var totalDonationsState = state.group().reduceSum(function(d) {
-		return d.total_donations;
+  var totalProperties;
+	var totalProperties = ndx.groupAll().reduceSum(function(d) {
+    totalProperties = totalProperties+1;
+    return totalProperties;
 	});
 
-	var totalDonationsGrade = gradeLevel.group().reduceSum(function(d) {
-		return d.grade_level;
+	var totalTypes = roomType.group().reduceSum(function(d) {
+		return d.room_type;
 	});
 
-	var totalDonationsFundingStatus = fundingStatus.group().reduceSum(function(d) {
-		return d.funding_status;
+	var totalRatings = ratingCheckin.group().reduceSum(function(d) {
+		return d.rating_checkin;
 	});
 
-
-
-	var netTotalDonations = ndx.groupAll().reduceSum(function(d) {return d.total_donations;});
-
-	//Define threshold values for data
-	var minDate = datePosted.bottom(1)[0].date_posted;
-	var maxDate = datePosted.top(1)[0].date_posted;
-
-console.log(minDate);
-console.log(maxDate);
+// 	//Define threshold values for data
+// 	var minDate = datePosted.bottom(1)[0].date_posted;
+// 	var maxDate = datePosted.top(1)[0].date_posted;
+//
+// console.log(minDate);
+// console.log(maxDate);
 
     //Charts
-	var dateChart = dc.lineChart("#date-chart");
-	var gradeLevelChart = dc.rowChart("#grade-chart");
+  var totalproperties = dc.numberDisplay("#total-properties");
+  var netDonations = dc.numberDisplay("#net-donations");
+  // var dateChart = dc.lineChart("#date-chart");
+  // var gradeLevelChart = dc.rowChart("#grade-chart");
 	var resourceTypeChart = dc.rowChart("#resource-chart");
-	var fundingStatusChart = dc.pieChart("#funding-chart");
-	var povertyLevelChart = dc.rowChart("#poverty-chart");
-	var totalProjects = dc.numberDisplay("#total-projects");
-	var netDonations = dc.numberDisplay("#net-donations");
-	var stateDonations = dc.barChart("#state-donations");
+	// var fundingStatusChart = dc.pieChart("#funding-chart");
+	// var povertyLevelChart = dc.rowChart("#poverty-chart");
+	// var stateDonations = dc.barChart("#state-donations");
 
 
   selectField = dc.selectMenu('#menuselect')
-        .dimension(state)
-        .group(stateGroup); 
+        .dimension(roomType)
+        .group(propertiesByType);
 
        dc.dataCount("#row-selection")
         .dimension(ndx)
         .group(all);
 
 
-	totalProjects
+	totalproperties
 		.formatNumber(d3.format("d"))
 		.valueAccessor(function(d){return d; })
 		.group(all);
@@ -88,73 +83,74 @@ console.log(maxDate);
 	netDonations
 		.formatNumber(d3.format("d"))
 		.valueAccessor(function(d){return d; })
-		.group(netTotalDonations)
+		.group(totalProperties)
 		.formatNumber(d3.format(".3s"));
 
-	dateChart
-		//.width(600)
-		.height(220)
-		.margins({top: 10, right: 50, bottom: 30, left: 50})
-		.dimension(datePosted)
-		.group(projectsByDate)
-		.renderArea(true)
-		.transitionDuration(500)
-		.x(d3.time.scale().domain([minDate, maxDate]))
-		.elasticY(true)
-		.renderHorizontalGridLines(true)
-    	.renderVerticalGridLines(true)
-		.xAxisLabel("Year")
-		.yAxis().ticks(6);
+	// dateChart
+	// 	//.width(600)
+	// 	.height(220)
+	// 	.margins({top: 10, right: 50, bottom: 30, left: 50})
+	// 	.dimension(datePosted)
+	// 	.group(propertiesByDate)
+	// 	.renderArea(true)
+	// 	.transitionDuration(500)
+	// 	.x(d3.time.scale().domain([minDate, maxDate]))
+	// 	.elasticY(true)
+	// 	.renderHorizontalGridLines(true)
+  //   	.renderVerticalGridLines(true)
+	// 	.xAxisLabel("Year")
+	// 	.yAxis().ticks(6);
 
 	resourceTypeChart
         //.width(300)
         .height(220)
-        .dimension(resourceType)
-        .group(projectsByResourceType)
+        .dimension(roomType)
+        .group(propertiesByType)
         .elasticX(true)
         .xAxis().ticks(5);
 
-	povertyLevelChart
-		//.width(300)
-		.height(220)
-        .dimension(povertyLevel)
-        .group(projectsByPovertyLevel)
-        .xAxis().ticks(4);
-
-	gradeLevelChart
-		//.width(300)
-		.height(220)
-        .dimension(gradeLevel)
-        .group(projectsByGrade)
-        .xAxis().ticks(4);
-
-  
-          fundingStatusChart
-            .height(220)
-            //.width(350)
-            .radius(90)
-            .innerRadius(40)
-            .transitionDuration(1000)
-            .dimension(fundingStatus)
-            .group(projectsByFundingStatus);
-
-
-    stateDonations
-    	//.width(800)
-        .height(220)
-        .transitionDuration(1000)
-        .dimension(state)
-        .group(totalDonationsState)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
-        .centerBar(false)
-        .gap(5)
-        .elasticY(true)
-        .x(d3.scale.ordinal().domain(state))
-        .xUnits(dc.units.ordinal)
-        .renderHorizontalGridLines(true)
-        .renderVerticalGridLines(true)
-        .ordering(function(d){return d.value;})
-        .yAxis().tickFormat(d3.format("s"));
+	// povertyLevelChart
+	// 	//.width(300)
+	// 	.height(220)
+  //       .dimension(povertyLevel)
+  //       .group(propertiesByPovertyLevel)
+  //       .xAxis().ticks(4);
+  //
+	// gradeLevelChart
+	// 	//.width(300)
+	// 	.height(220)
+  //       .dimension(gradeLevel)
+  //       .group(propertiesByGrade)
+  //       .xAxis().ticks(4);
+  //
+  //
+  //         fundingStatusChart
+  //           .height(220)
+  //           //.width(350)
+  //           .radius(90)
+  //           .innerRadius(40)
+  //           .transitionDuration(1000)
+  //           .dimension(fundingStatus)
+  //           .group(propertiesByFundingStatus);
+  //
+  //
+  //   stateDonations
+  //   	//.width(800)
+  //       .height(220)
+  //       .transitionDuration(1000)
+  //       .dimension(state)
+  //       .group(totalDonationsState)
+  //       .margins({top: 10, right: 50, bottom: 30, left: 50})
+  //       .centerBar(false)
+  //       .gap(5)
+  //       .elasticY(true)
+  //       .x(d3.scale.ordinal().domain(state))
+  //       .xUnits(dc.units.ordinal)
+  //       .renderHorizontalGridLines(true)
+  //       .renderVerticalGridLines(true)
+  //       .ordering(function(d){return d.value;})
+  //
+  //       .yAxis().tickFormat(d3.format("s"));
 
 
 
